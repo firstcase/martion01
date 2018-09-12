7c465271fdc0ef29aae794b913df6f6a056625e0
Eunha Jeong <kikicocoi79@gmail.com>
2018-09-12 21:43:33 +0900

    Initial Commit

---
+++ martion01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  martion01
+//
+//  Created by lucie on 2018. 9. 12..
+//  Copyright © 2018년 lucie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
