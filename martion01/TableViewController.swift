40628c87777ef0ddbdded0b82e3410579eab2334
Eunha Jeong <kikicocoi79@gmail.com>
2018-10-24 23:19:40 +0900

    테이블 뷰를 추가했습니다. 더 많은 테스트를 선택해서 할 수 있습니다.

---
+++ martion01/TableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TableViewController.swift
+//  martion01
+//
+//  Created by lucie on 23/10/2018.
+//  Copyright © 2018 lucie. All rights reserved.
+//
+
+import Foundation
+
+class TableViewController : UITableViewController  {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        if indexPath.row == 0 {
+            cell.textLabel?.text = "자기감시 테스트"
+        } else {
+            cell.textLabel?.text = "자존감 테스트"
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let t01 = self.storyboard!.instantiateViewController(withIdentifier: "TestNo1")
+            self.present(t01, animated: true)
+        } else if indexPath.row == 1 {
+            print("1")
+        }
+    }
+    
+}
+
